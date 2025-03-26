03cfd2f8a21d0feb35133a6996c6ea5631c7ad8a
graeme-macdonald-2 <macdonagr@students.dsbn.org>
2025-03-26 12:13:15 -0400

    rgrtjhrteuyjntjy

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+# Graeme M
+# Lists
+# March 26/25
+# This code creates 100 random numbers and prints the average
+import random
+import math
+def list(): return random.randint(1,100)
+lelist = [list() for i in range (100)]
+print(lelist)
+average = sum(lelist) / 100
+print(average)
